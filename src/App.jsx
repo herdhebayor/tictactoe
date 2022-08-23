d59d3222a1a5a92ae52f37589cce7c15961e8d84
herdhebayor <98285362+herdhebayor@users.noreply.github.com>
2022-08-23 07:47:49 -0700

    config file

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import btn from "./button";
+
+export default () => (
+  <>
+   <div className="btn-board">
+      <btn-btn value={0}/>
+   </div>
+     <div className="btn-board">
+
+   </div>
+     <div className="btn-board">
+
+   </div>
+  </>
+);
---
+++ src/button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const btn = () => (
+<>
+<button className="btn-btn"></button>
+</>)
+
+
+export default btn
